32728b30aac8e70eb46f94f76f9a90e668cc8516
Karpenya <karpenyalexa1993@gmail.com>
2022-08-05 02:15:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+Console.Write("Введите число ");
+string number = Console.ReadLine();
+char[] c = number.ToCharArray();
+Array.Reverse(c);
+int truenumber = Convert.ToInt32(number);
+string newc = String.Concat<char>(c);
+int truenumber2 = Convert.ToInt32(newc);
+if (truenumber == truenumber2)
+    Console.WriteLine("Это возмутимо но это полиндром");
+else
+    Console.WriteLine("Это возмутимо но это не полидром");
+
+
+
+
+
